a661460bd26323d6dff0cf360c5b5eedc01b77f0
ombhandari7989@gmail.com <ombhandari7989@gmail.com>
2024-02-24 01:18:45 +0530

    added

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-mern-be.onrender.com' });
+const API = axios.create({ baseURL: 'https://memories-mern-be.onrender.com/' });
 
 
 API.interceptors.request.use((req) => {
