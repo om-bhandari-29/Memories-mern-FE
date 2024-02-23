decfda5cb1aa98e8c8a5513e5bbc290da1fdce27
ombhandari7989@gmail.com <ombhandari7989@gmail.com>
2024-02-24 01:16:04 +0530

    removed _redirect code

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://memories-mern-be.onrender.com' });
 
+
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
