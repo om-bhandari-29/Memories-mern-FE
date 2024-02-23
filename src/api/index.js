aaa3caa8d8f8c09e9bd0b5d3d5db980362d6f743
ombhandari7989@gmail.com <ombhandari7989@gmail.com>
2024-02-24 00:29:27 +0530

    initial commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-be-1.onrender.com' });
+// const API = axios.create({ baseURL: 'https://memories-be-1.onrender.com' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
